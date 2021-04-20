18ceb75b4f067bdbfdffe6129089484af5755f50
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-04-20 15:07:26 +0530

    Created class and related items for Location Entity Task AB#59

---
+++ src/Bindu.Sampatti.Domain/Locations/Location.cs
@@ -0,0 +1,40 @@
+﻿using JetBrains.Annotations;
+using System;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Bindu.Sampatti.Locations
+{
+    public class Location : AuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool IsEnabled { get; set; }
+
+        private Location()
+        {
+            /* This constructor is for deserialization / ORM purpose */
+        }
+
+        internal Location(Guid id, [NotNull]string name, [CanBeNull]string notes, bool isEnabled):base(id)
+        {
+            Notes = notes;
+            IsEnabled = isEnabled;
+        }
+
+        internal Location ChangeName([NotNull]string newName)
+        {
+            SetName(newName);
+            return this;
+        }
+
+        private void SetName([NotNull]string newName)
+        {
+            Name = Check.NotNullOrWhiteSpace(
+                newName, 
+                nameof(newName), 
+                maxLength: LocationConsts.MaxNameLength);
+        }
+    }
+
+}
