2d8eb2d8d6a0d250675ea1f796c3efcbf6e3ad59
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-04-20 15:07:26 +0530

    Created class and related items for Location Entity Task #59

---
+++ src/Bindu.Sampatti.Domain.Shared/Locations/LocationConsts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bindu.Sampatti.Locations
+{
+    public static class LocationConsts
+    {
+        public const int MaxNameLength = 64;
+    }
+}
