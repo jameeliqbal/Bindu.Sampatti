f0b75991b0d192ade77de3b1824b011ea4fcd4c6
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-04-21 13:28:40 +0530

    created DTOs for Location AB#61

---
+++ src/Bindu.Sampatti.Application.Contracts/Locations/CreateLocationDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bindu.Sampatti.Locations
+{
+    public class CreateLocationDto 
+    {
+        [Required]
+        [StringLength(LocationConsts.MaxNameLength)]
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        [Required]
+        public bool IsEnabled { get; set; }
+    }
+}
