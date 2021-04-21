7bfc81a0aa674d6c93b36fe36274904c45e60e57
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-04-21 13:14:53 +0530

    created Application Service interface for 'Location' AB#62

---
+++ src/Bindu.Sampatti.Application.Contracts/Locations/ILocationAppService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Services;
+
+namespace Bindu.Sampatti.Locations
+{
+    public interface ILocationAppService : IApplicationService
+    {
+        Task<LocationDto> GetAsync(Guid id);
+        Task<PagedResultDto<LocationDto>> GetListAsync(GetLocationListDto input);
+        Task<LocationDto> locationDto(CreateLocationDto input);
+        Task UpdateAsnyc(Guid id, UpdateLocationDto input);
+        Task DeleteAsync(Guid id);
+    }
+}
