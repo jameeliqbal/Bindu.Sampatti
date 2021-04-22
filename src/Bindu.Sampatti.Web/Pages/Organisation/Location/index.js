b8b672cb37df52bfe4972c1872e364e7b5cbfe1f
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-04-22 17:28:28 +0530

    implemented listing functionality for Location

---
+++ src/Bindu.Sampatti.Web/Pages/Organisation/Location/index.js
@@ -0,0 +1,51 @@
+﻿$(function(){
+    var l = abp.localization.getResource("Sampatti");
+
+    var locationsDataTable = $("#LocationsTable").DataTable(
+        abp.libs.datatables.normalizeConfiguration({
+            serverSide: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: false,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(bindu.sampatti.locations.location.getList),
+            columnDefs: [
+                {
+                    title: l('Actions'),
+                    rowAction: {
+                        items:
+                            [
+                                {
+                                    text: l('Edit'),
+                                    //action: function (data) {
+
+                                    //}
+                                },
+                                {
+                                    text: l('Delete')
+                                }
+                            ]
+                    }
+                },
+                {
+                    title: l('LocationName'),
+                    data: "name"
+                },
+                {
+                    title: l('LocationStatus'),
+                    data: "isEnabled",
+                    render: function (data) {
+                        if (data == true)
+                            return "Active"
+                        else
+                            return "In-Active"
+                    }
+                }
+
+             
+            ]
+
+        })
+    ); //datatable
+
+});
