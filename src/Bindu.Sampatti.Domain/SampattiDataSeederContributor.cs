3452c79c3da278c0c3443b28cb7a4b18c49b5f3a
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-04 17:23:26 +0530

    Created EF Migration for Depot and added seed data AB#67

--- src/Bindu.Sampatti.Domain/SampattiDataSeederContributor.cs
+++ src/Bindu.Sampatti.Domain/SampattiDataSeederContributor.cs
@@ -1,4 +1,5 @@
-﻿using Bindu.Sampatti.Locations;
+﻿using Bindu.Sampatti.Depots;
+using Bindu.Sampatti.Locations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,10 +14,17 @@ namespace Bindu.Sampatti
     {
         private readonly ILocationRepository _locationRepo;
         private readonly LocationManager _locationManager;
-        public SampattiDataSeederContributor(ILocationRepository locationRepo, LocationManager locationManager)
+        private readonly IDepotRepository _depotRepository;
+        private readonly DepotManager _depotManager;
+
+        public SampattiDataSeederContributor(ILocationRepository locationRepo, LocationManager locationManager,
+                                                IDepotRepository depotRepository, DepotManager depotManager)
         {
             _locationRepo = locationRepo;
             _locationManager = locationManager;
+
+            _depotRepository = depotRepository;
+            _depotManager = depotManager;
         }
 
         public async Task SeedAsync(DataSeedContext context)
@@ -28,6 +36,19 @@ namespace Bindu.Sampatti
                 var locationThree = await _locationManager.CreateAsync("Location THREE", true, "some notes about three");
 
                 await _locationRepo.InsertManyAsync(new List<Location>() { locationOne, locationTwo, locationThree });
+
+            }
+
+            if (await _depotRepository.GetCountAsync() <= 0)
+            {
+                var locations = await _locationRepo.GetListAsync();
+ 
+                var depotOne = await _depotManager.CreateAsync("Depot ONE", locations[0].Id, "some notes about depot one", true);
+                var depotTwo = await _depotManager.CreateAsync("Depot TWO", locations[1].Id, "some notes about depot two", true);
+                var depotThree = await _depotManager.CreateAsync("Depot THREE", locations[2].Id, "some notes about depot three", true);
+                var depotFour = await _depotManager.CreateAsync("Depot FOUR", locations[1].Id, "some notes about depot four", true);
+
+                await _depotRepository.InsertManyAsync(new List<Depot> { depotOne, depotTwo, depotThree, depotFour});
 
             }
         }
