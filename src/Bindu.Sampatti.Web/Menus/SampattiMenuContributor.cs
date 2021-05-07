23f3ab8fbef6c2a2995bc0f09a65718629321327
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-07 17:59:02 +0530

    added menu for Depots AB#77 removed menu for Sections

--- src/Bindu.Sampatti.Web/Menus/SampattiMenuContributor.cs
+++ src/Bindu.Sampatti.Web/Menus/SampattiMenuContributor.cs
@@ -52,8 +52,8 @@ namespace Bindu.Sampatti.Web.Menus
             context.Menu.Items.Insert(4, menuOrg);
             menuOrg.AddItem(new ApplicationMenuItem(SampattiMenus.Employees, l["Menu:Organization.Employees"], "#"));
             menuOrg.AddItem(new ApplicationMenuItem(SampattiMenus.Departments, l["Menu:Organization.Departments"], "#"));
-            menuOrg.AddItem(new ApplicationMenuItem(SampattiMenus.Sections, l["Menu:Organization.Sections"], "#"));
             menuOrg.AddItem(new ApplicationMenuItem(SampattiMenus.Locations, l["Menu:Organization.Locations"], "/organisation/location"));
+            menuOrg.AddItem(new ApplicationMenuItem(SampattiMenus.Depots, l["Menu:Organization.Depots"], "/organisation/depot"));
 
         }
     }
