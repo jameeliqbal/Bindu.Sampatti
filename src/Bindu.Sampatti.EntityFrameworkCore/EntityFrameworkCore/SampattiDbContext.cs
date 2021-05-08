6b7bed6efc00ba68f4c3210ea5300a0664aaf7bf
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-08 13:01:16 +0530

    created dbset for Plant Entity and configured builder properties for the same AB#81

--- src/Bindu.Sampatti.EntityFrameworkCore/EntityFrameworkCore/SampattiDbContext.cs
+++ src/Bindu.Sampatti.EntityFrameworkCore/EntityFrameworkCore/SampattiDbContext.cs
@@ -24,8 +24,8 @@ namespace Bindu.Sampatti.EntityFrameworkCore
     {
         public DbSet<AppUser> Users { get; set; }
         public DbSet<Location> Locations { get; set; }
-
         public DbSet<Depot> Depots { get; set; }
+        public DbSet<Plants> Plants { get; set; }
 
         /* Add DbSet properties for your Aggregate Roots / Entities here.
          * Also map them inside SampattiDbContextModelCreatingExtensions.ConfigureSampatti
