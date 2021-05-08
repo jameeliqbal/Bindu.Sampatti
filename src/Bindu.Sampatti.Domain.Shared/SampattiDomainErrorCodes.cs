78133dbbfcaab76b784b3143c0e5e11478585321
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-08 12:50:40 +0530

    implemented domain service for plant entity AB#80

--- src/Bindu.Sampatti.Domain.Shared/SampattiDomainErrorCodes.cs
+++ src/Bindu.Sampatti.Domain.Shared/SampattiDomainErrorCodes.cs
@@ -5,5 +5,6 @@
         /* You can add your business exception error codes here, as constants */
         public const string LocationAlreadyExists = "Sampatti:00001";
         public const string DepotAlreadyExists = "Sampatti:00002";
-    }
+        public const string PlantAlreadyExists = "Sampatti:00003";
+   }
 }
---
+++ src/Bindu.Sampatti.Domain/Plants/IPlantRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace Bindu.Sampatti.Plants
+{
+    public interface IPlantRepository : IRepository<Plant, Guid>
+    {
+        Task<Plant> FindByNameAsync(string name);
+
+        Task<List<Plant>> GetListAsync(int skipCount, int maxResultCount, string sorting, string filter = null);
+    }
+}
