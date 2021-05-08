561809861d4a3f5fbae7bb57d875f72cb469f82b
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-08 16:11:35 +0530

    Created DTOs for Plant Entity in application layer AB#83

---
+++ src/Bindu.Sampatti.Application.Contracts/Plants/PlantDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Bindu.Sampatti.Plants
+{
+    public class PlantDto : AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public Guid LocationId { get; set; }
+        public string LocationName { get; set; }
+        public string Notes { get; set; }
+        public bool Status { get; set; }
+    }
+     
+}
