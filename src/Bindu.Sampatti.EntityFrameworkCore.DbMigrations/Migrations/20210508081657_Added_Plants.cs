675681f8b175b192d9910b7195a9912f643a31f4
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-08 15:41:42 +0530

    created ef migration and seeded data for Plant entity. Implemented repository for Plant Entity AB#81

---
+++ src/Bindu.Sampatti.EntityFrameworkCore.DbMigrations/Migrations/20210508081657_Added_Plants.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bindu.Sampatti.Migrations
+{
+    public partial class Added_Plants : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppPlants",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Location = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppPlants", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AppPlants_AppLocations_Location",
+                        column: x => x.Location,
+                        principalTable: "AppLocations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppPlants_Location",
+                table: "AppPlants",
+                column: "Location");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppPlants_Name",
+                table: "AppPlants",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppPlants");
+        }
+    }
+}
