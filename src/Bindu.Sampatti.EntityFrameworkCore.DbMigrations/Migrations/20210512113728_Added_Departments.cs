9e5abaffb8fab7c37c1e2be899f3455b3fb73d68
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-12 18:02:38 +0530

    created ef core migration for Department Entity AB#94

---
+++ src/Bindu.Sampatti.EntityFrameworkCore.DbMigrations/Migrations/20210512113728_Added_Departments.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bindu.Sampatti.Migrations
+{
+    public partial class Added_Departments : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppDepartments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Notes = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<bool>(type: "bit", nullable: false),
+                    ExtraProperties = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: true),
+                    CreationTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatorId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    LastModificationTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifierId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppDepartments", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AppDepartments_Name",
+                table: "AppDepartments",
+                column: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppDepartments");
+        }
+    }
+}
