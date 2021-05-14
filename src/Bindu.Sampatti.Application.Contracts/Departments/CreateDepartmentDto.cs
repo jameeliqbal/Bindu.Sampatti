72b2d8a0e2ed0b5ab147ec3464b28f1d09aea0a9
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-14 16:10:05 +0530

    Created DTOs for Department Entity AB#96

---
+++ src/Bindu.Sampatti.Application.Contracts/Departments/CreateDepartmentDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bindu.Sampatti.Departments
+{
+    class CreateDepartmentDto
+    {
+        [Required]
+        [StringLength(DepartmentConsts.MaxNameLength)]
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Status { get; set; }
+    }
+}
