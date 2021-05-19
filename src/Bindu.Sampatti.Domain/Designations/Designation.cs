205494c29e73bdcb531fc6a928c932e08155c95b
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-19 07:36:21 +0530

    created class for Designation entity AB#109

---
+++ src/Bindu.Sampatti.Domain/Designations/Designation.cs
@@ -0,0 +1,42 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Bindu.Sampatti.Designations
+{
+    public class Designation : AuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public bool Status { get; set; }
+
+        private Designation()
+        {
+            /* This constructor is for deserialization / ORM purpose */
+        }
+
+        internal Designation(Guid id, [NotNull] string name, [CanBeNull] string notes, bool status) : base(id)
+        {
+            SetName(name);
+            Notes = notes;
+            Status = status;
+        }
+
+        internal Designation ChangeName([NotNull] string newName)
+        {
+            SetName(newName);
+            return this;
+        }
+
+
+        private void SetName([NotNull] string newName)
+        {
+            Name = Check.NotNullOrWhiteSpace(newName, nameof(newName), DesignationConsts.MaxNameLength);
+        }
+    }
+}
