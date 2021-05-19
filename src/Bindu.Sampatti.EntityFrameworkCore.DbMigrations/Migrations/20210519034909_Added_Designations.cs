66a26d3d380bdfe49fdf9608922ebd312d8cb9af
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-19 11:35:01 +0530

    added EF Core Migration for Designation Entity AB#111

---
+++ src/Bindu.Sampatti.EntityFrameworkCore.DbMigrations/Migrations/20210519034909_Added_Designations.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Bindu.Sampatti.Migrations
+{
+    public partial class Added_Designations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppDepartments",
+                table: "AppDepartments");
+
+            migrationBuilder.RenameTable(
+                name: "AppDepartments",
+                newName: "AppDesignations");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AppDepartments_Name",
+                table: "AppDesignations",
+                newName: "IX_AppDesignations_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppDesignations",
+                table: "AppDesignations",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AppDesignations",
+                table: "AppDesignations");
+
+            migrationBuilder.RenameTable(
+                name: "AppDesignations",
+                newName: "AppDepartments");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AppDesignations_Name",
+                table: "AppDepartments",
+                newName: "IX_AppDepartments_Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AppDepartments",
+                table: "AppDepartments",
+                column: "Id");
+        }
+    }
+}
