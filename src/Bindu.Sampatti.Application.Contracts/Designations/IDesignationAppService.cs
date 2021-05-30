b0e3227611d38cee2a7a9cadbdf17cfa1d25cd29
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-05-30 09:09:01 +0530

    created application service interface for Designation Entity AB#113

---
+++ src/Bindu.Sampatti.Application.Contracts/Designations/IDesignationAppService.cs
@@ -0,0 +1,21 @@
+﻿using Bindu.Sampatti.Departments;
+using System;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Bindu.Sampatti.Designations
+{
+    public interface IDesignationAppService : IApplicationService
+    {
+        Task<DesignationDto> GetAsync(Guid id);
+
+        Task<PagedResultDto<DesignationDto>> GetListAsync(GetDepartmentListDto input);
+
+        Task<DesignationDto> CreateAsync(CreateDepartmentDto input);
+
+        Task UpdateAsync(Guid id, UpdateDepartmentDto input);
+
+        Task DeleteAsync(Guid id);
+    }
+}
