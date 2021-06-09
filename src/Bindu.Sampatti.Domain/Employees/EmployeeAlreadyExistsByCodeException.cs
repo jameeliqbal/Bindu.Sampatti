ee9b1780594c429fcb39931d6536887d37c5d1d3
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-06-09 17:11:36 +0530

    Implemented domain service for Employee Entity AB#125

---
+++ src/Bindu.Sampatti.Domain/Employees/EmployeeAlreadyExistsByCodeException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp;
+
+namespace Bindu.Sampatti.Employees
+{
+    public class EmployeeAlreadyExistsByCodeException : BusinessException
+    {
+        public EmployeeAlreadyExistsByCodeException(string code) : base(SampattiDomainErrorCodes.EmployeeByCodeAlreadyExists)
+        {
+            WithData("code", code);
+        }
+    }
+}
