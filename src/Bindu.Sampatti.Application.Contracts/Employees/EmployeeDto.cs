e42f48bb23d7c83564d245eaba767808aa00fe96
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-06-10 17:31:28 +0530

    created DTOs for Employee Entity AB#127

---
+++ src/Bindu.Sampatti.Application.Contracts/Employees/EmployeeDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Bindu.Sampatti.Employees
+{
+    public class EmployeeDto:AuditedEntityDto<Guid>
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public Guid Designation { get; set; }
+
+        public Guid Department { get; set; }
+
+        public string Notes { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ src/Bindu.Sampatti.Application.Contracts/Employees/UpdateEmployeeDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bindu.Sampatti.Employees
+{
+    public class UpdateEmployeeDto
+    {
+        [Required]
+        [StringLength(EmployeeConsts.MaxNameLength)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(EmployeeConsts.MaxCodeLength)]
+        public string Code { get; set; }
+
+        public Guid Designation { get; set; }
+
+        public Guid Department { get; set; }
+
+        public string Notes { get; set; }
+        public bool Status { get; set; }
+    }
+}
