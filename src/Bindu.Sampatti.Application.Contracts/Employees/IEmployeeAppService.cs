e15e59af7fe95f256d214b2d5bf65a35784a9b19
Jameel Saroash Iqbal <jameeliqbal@outlook.com>
2021-06-10 17:39:58 +0530

    created application service interface for Employee entity AB#128

---
+++ src/Bindu.Sampatti.Application.Contracts/Employees/IEmployeeAppService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Bindu.Sampatti.Employees
+{
+    public interface IEmployeeAppService : IApplicationService
+    {
+        Task<EmployeeDto> GetAsync(Guid id);
+        Task<EmployeeDto> GetAsync(string employeeCode);
+        Task<PagedResultDto<EmployeeDto>> GetListAsync(GetEmployeeListDto input);
+        Task<EmployeeDto> CreateAsync(CreateEmployeeDto input);
+        Task UpdateAsync(Guid id, UpdateEmployeeDto input);
+        Task DeleteAsync(Guid id);
+        Task<ListResultDto<DesignationLookupDto>> GetDesignationLookupAsync();
+        Task<ListResultDto<DepartmentLookupDto>> GetDepartmentLookupAsync();
+
+    }
+}
